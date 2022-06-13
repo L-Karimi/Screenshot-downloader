53e0e7a74853dced15f8d0c39886deb71f32d97c
L-Karimi <luciahkei101@gmail.com>
2022-06-13 19:02:25 +0300

    modified scripts.js

--- scripts.js
+++ scripts.js
@@ -1,35 +1,49 @@
-async function from loadImage(){
-
-
-    let formUrl = document.getElementById("url").ariaValueMax;
-    let token ="3SBR33M-YC3MX0V-K60NH30-PKP88DQ"
-    let url = "https://shot.screenshotapi.net/screenshot?token=${token}&url=${formUrl}"
-    const responce =  await fetch(url)
-    const onject = await Response.json()
-
-    let newIMage =document.createElement("img")
-    newIMage.className= "screenshot"
-
-    newIMage.setAttribute("src",Object.screenshot);
-    let container = document.querySelector('.image');
-    let reference = document.querySelector('.reference')
-
-  if (document.images.length >= 1 ) {
-      let existingImg = document.querySelector('.screenshot')
-      container.replaceChild(newImg, existingImg)
-  } else {
-      container.insertBefore(newImg, reference)
-  }
-
-}
-let button = document.getElementById('btn')
-button.addEventListener("click",async(event)=>){
-    event.preventDefault()
-    try{
-        loadImage()
-    }catch(e){
-        console.log("Error loading page!");
-        console.log(e);
-    }
+async function loadImage() {
+    // get url value from from field and token from dashboard. Construct URL 
    
-}
+     let formUrl = document.getElementById('url').value
+     let token = "3SBR33M-YC3MX0V-K60NH30-PKP88DQ"
+     let url = `https://shot.screenshotapi.net/screenshot?token=${token}&url=${formUrl}`
+   
+    // Make a get request to screenshotnet API and get response object
+     const response = await fetch(url)
+     const object = await response.json()
+   
+   
+     //create a new image element
+     let newImg = document.createElement('img')
+   
+     // set class on that element
+     newImg.className= 'screenshot'
+   
+    // set src property with the images' url from response object
+     newImg.setAttribute('src', object.screenshot)
+   
+    // get the nodes where you want to insert the new image
+     let container = document.querySelector('.image')
+     let reference = document.querySelector('.reference')
+   
+     if (document.images.length >= 1 ) {
+        let existingImg = document.querySelector('.screenshot')
+        container.replaceChild(newImg, existingImg)
+    } else {
+        container.insertBefore(newImg, reference)
+    }
+  
+  }
+  let button = document.getElementById('btn')
+
+// add event listener, run an async function when button gets clicked
+
+button.addEventListener("click", async (event) => {
+
+    // prevent from submission
+    event.preventDefault()
+
+    try {
+      loadImage()
+    } catch(e) {
+      console.log("Error!");
+      console.log(e);
+    }
+})
